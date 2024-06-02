5660e6c1d16630dae0cc1e92b20e795ead01fc4e
vanshika-max <vanshikaparjapati1@gmail.com>
2024-06-03 00:17:46 +0530

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+
+import "./App.css";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom";
+import { combineSlices } from "@reduxjs/toolkit";
+import Loader from "./components/Loader";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData: userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+
+  return !loading ? (
+    <div className="min-h-screen w-full flex flex-wrap justify-center bg-gray-200">
+      <div className="w-full block">
+        <Header />
+        <Outlet />
+        <Footer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  ) : (
+    <Loader />
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,72 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import store from "./store/store.js";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthLayout, Login } from "./components/index.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import Home from "./pages/Home.jsx";
+import AddPost from "./pages/AddPost";
+import Signup from "./pages/Signup";
+import EditPost from "./pages/EditPost";
+import Post from "./pages/Post";
+import AllPosts from "./pages/AllPosts";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+
+      <BrowserRouter >
+        <Routes>
+          <Route path="/" element={<App />} >
+
+            <Route index element={<Home /> }/>
+
+            <Route
+              path="login"
+              element={
+                <AuthLayout authentication={false}>
+                  <Login />
+                </AuthLayout>
+              }
+            />
+            <Route
+              path="signup"
+              element={
+                <AuthLayout authentication={false}>
+                  <Signup />
+                </AuthLayout>
+              }
+            />
+            <Route
+              path="all-posts"
+              element={
+                <AuthLayout authentication>
+                  <AllPosts />
+                </AuthLayout>
+              }
+            />
+            <Route
+              path="add-post"
+              element={
+                <AuthLayout authentication>
+                  <AddPost />
+                </AuthLayout>
+              }
+            />
+            <Route
+              path="edit-post/:slug"
+              element={
+                <AuthLayout authentication>
+                  <EditPost />
+                </AuthLayout>
+              }
+            />
+            <Route path="post/:slug" element={<Post />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
